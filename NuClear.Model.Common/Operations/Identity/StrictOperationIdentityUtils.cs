5bdd3f83dd6f459759c7b0ec25378fb2f7b99bf6
denisivan0v <ivanoff.denis@gmail.com>
2014-11-07 18:05:07 +0600

    AsUriSegment method

---
+++ NuClear.Model.Common/Operations/Identity/StrictOperationIdentityUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace NuClear.Model.Common.Operations.Identity
+{
+    public static class StrictOperationIdentityUtils
+    {
+        public static string AsUriSegment(this StrictOperationIdentity strictOperationIdentity)
+        {
+            var identityType = strictOperationIdentity.OperationIdentity.GetType();
+            return identityType.Name +
+                (strictOperationIdentity.OperationIdentity.IsNonCoupled()
+                       ? string.Empty
+                       : ("/" + string.Join("/", strictOperationIdentity.Entities.Select(x => x.ToString()))));
+        }
+    }
+}
