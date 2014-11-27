77d66f29f3fc3fcbe00a746c9d0a1918f0ed22e3
Denis Ivanov <d.ivanov@2gis.ru>
2014-11-27 13:18:30 +0600

    IEntityType.ToStringLocalized extension method added

--- NuClear.Model.Common/Entities/EntityTypeExtensions.cs
+++ NuClear.Model.Common/Entities/EntityTypeExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace NuClear.Model.Common.Entities
+﻿using System.Globalization;
+using System.Resources;
+
+namespace NuClear.Model.Common.Entities
 {
     public static class EntityTypeExtensions
     {
@@ -10,6 +13,20 @@
         public static EntityTypeNone None(this EntityType entityType)
         {
             return EntityTypeNone.Instance;
+        }
+
+        public static string ToStringLocalized(this IEntityType value, ResourceManager resourceManager, CultureInfo cultureInfo)
+        {
+            var type = value.GetType();
+            var name = value.Description;
+
+            if (resourceManager == null)
+            {
+                return name;
+            }
+
+            var localizedName = resourceManager.GetString(type.Name + name, cultureInfo);
+            return localizedName ?? name;
         }
     }
 }
