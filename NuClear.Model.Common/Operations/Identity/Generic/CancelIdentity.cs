8d92927d4127e6752747545c50bd170da3161a75
Denis Ivanov <d.ivanov@2gis.ru>
2015-03-24 11:21:11 +0600

    new operation identities added

---
+++ NuClear.Model.Common/Operations/Identity/Generic/CancelIdentity.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace NuClear.Model.Common.Operations.Identity.Generic
+{
+    [DataContract]
+    public class CancelIdentity : OperationIdentityBase<CancelIdentity>, IEntitySpecificOperationIdentity
+    {
+        public override int Id
+        {
+            get
+            {
+                return OperationIdentityIds.CancelIdentity;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Cancel";
+            }
+        }
+    }    
+}
---
+++ NuClear.Model.Common/Operations/Identity/Generic/ReopenIdentity.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace NuClear.Model.Common.Operations.Identity.Generic
+{  
+    [DataContract]
+    public class ReopenIdentity : OperationIdentityBase<ReopenIdentity>, IEntitySpecificOperationIdentity
+    {
+        public override int Id
+        {
+            get
+            {
+                return OperationIdentityIds.ReopenIdentity;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Reopen";
+            }
+        }
+    }    
+}
