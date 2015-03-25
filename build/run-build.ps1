5b2613ce27087e3151914631e2e2b6bcf3be9128
Denis Ivanov <d.ivanov@2gis.ru>
2015-03-25 16:20:31 +0600

    1.1.1

--- build/run-build.ps1
+++ build/run-build.ps1
@@ -17,7 +17,7 @@ $ErrorActionPreference = 'Stop'
 #------------------------------
 cls
 
-$Properties.GlobalVersion = '1.1.0'
+$Properties.GlobalVersion = '1.1.1'
 
 $Properties.BuildFile = Join-Path $PSScriptRoot 'default.ps1'
 $Properties.Dir = @{
