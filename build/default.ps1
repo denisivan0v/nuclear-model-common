1aff2711848ae89489d5ab018c83a5272ffc1d2d
m.pashuk <m.pashuk@2gis.ru>
2015-04-07 19:27:21 +0600

    nuget

--- build/default.ps1
+++ build/default.ps1
@@ -28,32 +28,6 @@ Task Update-AssemblyInfo {
 	Update-AssemblyInfo $assemblyInfos
 }
 
-Task Build-NuGet -depends Set-BuildNumber, Update-AssemblyInfo {
-
-	$SolutionRelatedAllProjectsDir = '.'
-
-	$tempDir = Join-Path $global:Context.Dir.Temp 'NuGet'
-	if (!(Test-Path $tempDir)){
-		md $tempDir | Out-Null
-	}
-
-	Create-NuspecFiles $SolutionRelatedAllProjectsDir
-
-	$projects = Find-Projects $SolutionRelatedAllProjectsDir -Exclude '*.Tests.csproj'
-	
-	# bug (https://nuget.codeplex.com/workitem/4013) NuGet fails to extract properties from PCL project
-	# bug will be fixed in NuGet 3.0
-	$Properties = @{
-		'Version' = (Get-Version).SemanticVersion
-		'Author' = '2GIS'
-		'Description' = 'This is Release version of assembly'
-	}
-	
-	Build-PackagesFromProjects $projects $tempDir $Properties
-	
-	Publish-Artifacts $tempDir 'NuGet'
-}
-
 Task Build-NuGetPackages -depends Set-BuildNumber, Update-AssemblyInfo {
 
     $SolutionRelatedAllProjectsDir = '.'
@@ -64,28 +38,14 @@ Task Build-NuGetPackages -depends Set-BuildNumber, Update-AssemblyInfo {
     }
 
     $projects = Find-Projects $SolutionRelatedAllProjectsDir -Filter '*.nuproj'
-
-    # bug (https://nuget.codeplex.com/workitem/4013) NuGet fails to extract properties from PCL project
-    # bug will be fixed in NuGet 3.0
-    $Properties = @{
-        'Version' = (Get-Version).SemanticVersion
-        'Author' = '2GIS'
-        'Description' = 'This is Release version'
-    }
-
-    Build-PackagesFromNuProjs $projects $tempDir $Properties
+	Build-PackagesFromNuProjs $projects $tempDir
 
     Publish-Artifacts $tempDir 'NuGet'
 }
 
 Task Deploy-NuGet {
 	$artifactName = Get-Artifacts 'NuGet'
-
-	$source = 'http://nuget.2gis.local'
-	$apiKey = ':enrbq rjl'
-
-	$packges = Get-ChildItem $artifactName -Include '*.nupkg' -Exclude '*.symbols.nupkg' -Recurse
-	Deploy-Packages $packges $source $apiKey
+	Deploy-Packages $artifactName
 }
 
 Task Run-UnitTests -depends Set-BuildNumber, Update-AssemblyInfo{
